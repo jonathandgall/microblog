778bb59f06a2e27474bfa86ea6f61fd7f7ebde61
Jonathan Gall <odajay@gmail.com>
2018-05-31 14:06:10 +0200

    Implementing Ajax Server-side to receive the data, translate and jsonified it and return to the client

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -8,6 +8,7 @@ from datetime import datetime
 from app.email import send_password_reset_email
 from flask_babel import _, get_locale
 from guess_language import guess_language
+from app.translate import translate
 
 @app.before_request
 def before_request():
@@ -169,3 +170,10 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return(jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])}))
