4497d3351c68a51b59cc8c0e1db7d397b4225843
Jonathan Gall <odajay@gmail.com>
2018-06-01 15:53:11 +0200

    Refactoring in main directory and preparing instancing in a a current_app

--- app/email.py
+++ app/email.py
@@ -1,8 +1,7 @@
 from flask_mail import Message
-from app import mail, app
-from flask import render_template
+from app import mail
 from threading import Thread
-from flask_babel import _
+from flask import current_app
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -12,6 +11,5 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
-
-
+    Thread(target=send_async_email, 
+           args=(current_app._get_current_object(), msg)).start()
