2f93027b71f8a0675a8644e40ccc6da81d393fcc
Jonathan Gall <odajay@gmail.com>
2018-06-03 01:38:05 +0200

    Starting the implementation of full text search

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as l_
+from elasticsearch import Elasticsearch
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -32,6 +33,8 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
 
     from app.errors import bp as errors_bp  # noqa: F401
     app.register_blueprint(errors_bp)
--- app/models.py
+++ app/models.py
@@ -62,13 +62,13 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            current_app.config['SECRET_KEY'], 
+            current_app.config['SECRET_KEY'],
             algorithm='HS256').decode('utf-8')
 
         @staticmethod
         def verify_reset_password_token(token):
             try:
-                id = jwt.decode(token, current_app.config['SECRET_KEY'], 
+                id = jwt.decode(token, current_app.config['SECRET_KEY'],
                                 algorithms=['HS256'])['reset_password']
 
             except BaseException:
@@ -76,6 +76,7 @@ class User(UserMixin, db.Model):
             return User.query.get(id)
 
 class Post(db.Model):
+    __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
